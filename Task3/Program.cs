1ab9072e4dd957a925ebfb30345fe86bf54eade7
Nata <lalliana@list.ru>
2022-12-17 21:34:25 +0300

    Task 3 solved

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число от 1 до  7");
+int a = int.Parse(ReadLine()!);
+
+if(a > 7)
+
+{
+    WriteLine("Введено неверное число");
+}
+    if (a == 1)
+    {
+         WriteLine("День недели - понедельник");
+    }
+    if (a == 2)
+    {
+        WriteLine("День недели - вторник");
+    }
+    if (a == 3)
+    {
+        WriteLine("День недели - среда");
+    }
+    if (a == 4)
+    {
+        WriteLine("День недели - четверг");
+    }
+    if (a == 5)
+    {
+        WriteLine("День недели - пятница");
+    }
+    if (a == 6)
+    {
+        WriteLine("День недели - суббота");
+    }
+    if (a == 7)
+    {
+        WriteLine("День недели - воскресенье");
+    }
+
+ 
