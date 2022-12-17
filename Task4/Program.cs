4caf6fcdcca430a54a67e1e21b00a0e05d0820a8
Nata <lalliana@list.ru>
2022-12-17 22:03:46 +0300

    Task 4 solved

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+int n=-a;
+
+while (n <= a)
+{
+    Write ($"{n}");
+    n = n + 1;
+}
