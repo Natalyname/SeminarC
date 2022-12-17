34b35a697f5f4f19cecb56eb6558e4e6f36297a5
Nata <lalliana@list.ru>
2022-12-17 22:16:22 +0300

    Task 5 solved

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear ();
+
+WriteLine("Введите число от 100 до 999");
+int a = int.Parse(ReadLine()!);
+
+WriteLine($"Последняя цифра числа {a} равна {a % 10}");
