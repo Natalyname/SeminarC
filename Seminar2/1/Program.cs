ca6056b9a5b8a4163c3ac2be025f83a1352a89f9
Nata <lalliana@list.ru>
2022-12-21 17:26:06 +0300

    Task №9 added

--- Seminar2/1/Program.cs
+++ Seminar2/1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+int number = new Random().Next(10, 100);
+WriteLine(number);
+
+WriteLine(number/10>number%10?number/10:number%10);
+
+// int a1 = number/10;
+// int a2 = number%10;
+
+// if (a1>a2)
+// {
+//     WriteLine(a1);
+// }
+// else
+// {
+//     WriteLine(a2);
+// }
