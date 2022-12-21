a80a33e9808f7580e1024ee94568d5a3ac093bc9
Nata <lalliana@list.ru>
2022-12-21 18:07:17 +0300

    Task № 12 added

--- Seminar2/3/Program.cs
+++ Seminar2/3/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите два числа: ");
+int a1 = int.Parse(ReadLine()!);
+int a2 = int.Parse(ReadLine()!);
+
+WriteLine(a1%a2==0? "Первое число кратно второму" : $"Остаток от деления равен {a1 % a2}");
