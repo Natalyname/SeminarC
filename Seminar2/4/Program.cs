e478b9dc2bb9312f8bd7b2c8b89a4904164dcd2e
Nata <lalliana@list.ru>
2022-12-21 19:00:27 +0300

    Задача на сравнение чисел добавлена

--- Seminar2/4/Program.cs
+++ Seminar2/4/Program.cs
@@ -1,4 +1,10 @@
-﻿using static System.Console;
+﻿//Программа принимает на вход 2 числа, на выходе выдает только знак сравнения этих 2 чисел.
+
+using static System.Console;
 Clear();
 
+WriteLine("Введите два числа: ");
+int a1 = int.Parse(ReadLine()!);
+int a2 = int.Parse(ReadLine()!);
 
+WriteLine(a1==a2 ? "числа равны" : a1>a2 ? ">" : "<");
