9adc0fe526c6325bf366b40d9ffd24c8986566c9
Nata <lalliana@list.ru>
2022-12-21 22:05:13 +0300

    Задача № 16 добавлена

--- Seminar2/6/Program.cs
+++ Seminar2/6/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите два числа: ");
+int a1 = int.Parse(ReadLine()!);
+int a2 = int.Parse(ReadLine()!);
+
+WriteLine(a1*a1==a2 || a2*a2==a1? "Одно число является квадратом другого" : "не являются");
