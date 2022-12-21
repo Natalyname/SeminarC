c4fe27b2bb10f9c38bfa5476c926d5310be2708f
Nata <lalliana@list.ru>
2022-12-21 22:30:39 +0300

    Задача №16 обновлено решение

--- Seminar2/6/Program.cs
+++ Seminar2/6/Program.cs
@@ -2,7 +2,8 @@
 Clear();
 
 WriteLine("Введите два числа: ");
-int a1 = int.Parse(ReadLine()!);
-int a2 = int.Parse(ReadLine()!);
+int a1 = Convert.ToInt32(ReadLine());
+int a2 = Convert.ToInt32(ReadLine());
 
-WriteLine(a1*a1==a2 || a2*a2==a1? "Одно число является квадратом другого" : "не являются");
+WriteLine(a1*a1==a2 ? $"Число {a2} является квадратом числа {a1}" 
+: a2*a2==a1 ? $"Число {a1} является квадратом числа {a2}" : "Ни одно из чисел не является квадратом другого");
