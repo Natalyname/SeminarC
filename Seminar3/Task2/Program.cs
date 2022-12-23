1474bd6cba34ba858fe21a17ee60c4fd1e67d82d
Nata <lalliana@list.ru>
2022-12-23 17:54:25 +0300

    задача на дни недели с помощью switch case added

--- Seminar3/Task2/Program.cs
+++ Seminar3/Task2/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//По номеру дня недели вывести его название, используя switch case.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите номер дня недели: ");
+int a = int.Parse(ReadLine()!);
+
+switch (a)
+{
+    case 1:
+    {
+        WriteLine($"{a = 1} - Понедельник");
+        break;
+    }
+    case 2:
+    {
+        WriteLine($"{a = 2} - Вторник");
+        break;
+    }
+    case 3:
+    {
+        WriteLine($"{a = 3} - Среда");
+        break;
+    }
+    case 4:
+    {
+        WriteLine($"{a = 4} - Четверг");
+        break;
+    }
+    case 5:
+    {
+        WriteLine($"{a = 5} - Пятница");
+        break;
+    }
+    case 6:
+    {
+        WriteLine($"{a = 6} - Суббота");
+        break;
+    }
+    case 7:
+    {
+        WriteLine($"{a = 7} - Воскресенье");
+        break;
+    }
+    default:
+    {
+        WriteLine("Неправильно введена цифра дня недели");
+        break;
+    }
+}
