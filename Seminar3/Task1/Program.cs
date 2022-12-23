4114644ff4a17e6295e84b1fc6ed11db12208495
Nata <lalliana@list.ru>
2022-12-23 16:30:33 +0300

    задача о четвертях добавлена

--- Seminar3/Task1/Program.cs
+++ Seminar3/Task1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите номер четверти: ");
+int i = int.Parse(ReadLine()!);
+
+switch (i)
+{
+    case 1:
+    {
+        WriteLine("x > 0 y > 0");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("x < 0 y > 0");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("x < 0 y < 0");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("x > 0 y < 0");
+        break;
+    }
+    default:
+    {
+        WriteLine("Ошибка");
+        break;
+    }
+}
