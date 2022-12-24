9efe1460a93b9620609e4514dd0f6e86fd90d8c6
Nata <lalliana@list.ru>
2022-12-24 16:04:59 +0300

    задача про токарей добавлена

--- Seminar3/Task4/Program.cs
+++ Seminar3/Task4/Program.cs
@@ -1,4 +1,15 @@
 ﻿using static System.Console;
 Clear();
 
+WriteLine("Введите количество деталей за смену: ");
+int quantity = int.Parse(ReadLine()!);
+int result = quantity%6;
 
+if (result == 0)
+{
+ WriteLine($"Младшие токари сделали по {quantity/6} шт., старший - {quantity/6*4} шт.");
+}
+else 
+{
+    WriteLine("Старший токарь ошибся");
+}
