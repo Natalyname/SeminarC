57893f00b8ecdf0a6d8066a49a0cef1404521f6a
Nata <lalliana@list.ru>
2022-12-25 23:32:24 +0300

    Task added

---
+++ Seminar3/Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+int i = 1;
+Write("Введите число для вывода таблицы квадратов от 1 до числа: ");
+int.TryParse(ReadLine(), out int n);
+
+while (i < n+1)
+{
+    WriteLine($"Квадрат числа {i} = {Math.Pow(i,2)}");
+    i++;
+}
