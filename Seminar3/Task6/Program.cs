cd120959b315a4cca8786432016c372a03fa4dff
Nata <lalliana@list.ru>
2022-12-25 16:45:29 +0300

    Задача о расстоянии между 2 точками в 2D пространстве добавлена

--- Seminar3/Task6/Program.cs
+++ Seminar3/Task6/Program.cs
@@ -1,4 +1,16 @@
 ﻿using static System.Console;
 Clear();
 
+WriteLine("Введите координаты точки А: ");
+Write("x = ");
+int.TryParse(ReadLine(), out int ax);
+Write("y = ");
+int.TryParse(ReadLine(), out int ay);
+WriteLine("Введите координаты точки B: ");
+Write("x = ");
+int.TryParse(ReadLine(), out int bx);
+Write("y = ");
+int.TryParse(ReadLine(), out int by);
 
+double length = Math.Sqrt(Math.Pow((ax-bx),2)+Math.Pow((ay-by),2));
+WriteLine($"{length:f2}");
