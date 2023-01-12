86786320ed95a0df130137ef2a37eb95beade03e
Nataly <natali@MacBook-Air-Nataly.local>
2023-01-12 23:22:27 +0300

    Добавлена задача на количество цифр в числе

---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int Count = Numbers(number);
+WriteLine($"Количестве цифр в числe {number} - {Count}");
+
+int Numbers(int A)
+{
+    int result = 0;
+    while (A>0)
+    {
+        A=A/10;
+        result++;
+    }
+    return result;
+}
