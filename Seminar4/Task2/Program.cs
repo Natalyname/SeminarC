df0aa0ca9fc0c9c64eb98cd8a3650f25606c2b40
Nataly <natali@MacBook-Air-Nataly.local>
2023-01-13 19:15:42 +0300

    Задача на сууму чисел от 1 до А добавлена

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А
+
+using static System.Console;
+Clear();
+
+Write ("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSum(number);
+WriteLine($"Сумма чисел от 1 до {number} = {sum}");
+
+int GetSum (int A)
+{
+    int result=0;
+    while (A>0)
+    {
+        result+=A;
+        A--;
+    }
+    return result;
+}
+
