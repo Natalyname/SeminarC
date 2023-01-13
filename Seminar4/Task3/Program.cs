836f68aa5995740b0de81acb7b7221428061aa25
Nataly <lalliana@list.ru>
2023-01-13 22:29:36 +0300

    Добавлена задача о произведении чисел от 1 до N

---
+++ Seminar4/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int multy = GetMulty(number);
+WriteLine($"Произведение чисел от 1 до {number} = {multy}");
+
+int GetMulty (int A)
+{
+    int result=1;
+    while (A>0)
+    {
+    result *=A;
+    A --;
+    }
+    return result;
+}
