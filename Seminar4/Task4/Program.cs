4a29d76af7db0cc5241a2509503bbb15551cfef5
Nataly <lalliana@list.ru>
2023-01-14 00:01:27 +0300

    Добавлена задача на сумму чисел, на которые число N делится без остатка

--- Seminar4/Task4/Program.cs
+++ Seminar4/Task4/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N, и выдает сумму чисел, на которые число N делится
+//без остатка.
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSum (number);
+WriteLine($"Сумма равна {sum}");
+
+    //int GetSum (int N)
+    //{
+    //int result=0;
+    //for (int i=1; i<=N; i++)
+    //{
+      //if (N%i ==0)
+      //{
+        //result+=i;
+      //}
+      //return result;
+      //}
+
+                     //ИЛИ:
+                     
+      int GetSum (int N)
+      {
+      int result=0;
+      int i=1;
+      while (i<=N)
+      {
+        if (N%i==0)
+        {
+            result+=i;
+        }
+        i++;
+      }
+        return result;
+    }
+    
+    //return result;
