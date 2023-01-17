23fcd51d98220d20d19fd6d069fab314f7bd3fab
Nataly <lalliana@list.ru>
2023-01-17 16:38:44 +0300

    Задача на 12 случ. эл-в массива от -9 до 9.Найти сумму полож. и отриц. эл-в.

---
+++ Seminar5/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+
+using static System.Console;
+Clear();
+
+Write("Введите размер массива, min и max через пробел: ");
+string [] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int [] array=GetArray(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]));
+PrintArray(array);
+WriteLine();
+
+int negativeSum = 0;
+int positiveSum = 0;
+
+foreach (int a in array)
+{
+    negativeSum += a < 0 ? a : 0;
+    positiveSum += a > 0 ? a : 0;
+}
+WriteLine($"{negativeSum}  {positiveSum}");
+
+
+int[] GetArray (int size, int MinValue, int MaxValue) //метод формирования массива
+{
+    int[] resultArray = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray [i] = rnd.Next(MinValue, MaxValue+1);
+    }
+    return resultArray;
+}
+
+
+void PrintArray(int[] inArray)   //Метод красиво выврдит массив в квадратных скобках через запятые.
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length-1; i++)
+    {
+       Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
