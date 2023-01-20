846f36fa99df810ea2bc6c65ef4bbe5d1b0cd80a
Nataly <lalliana@list.ru>
2023-01-20 16:56:05 +0300

    Программа, определяющая пристствует ли заданное число в массиве

---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 33:** Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//3; массив [6, 7, 19, 345, 3] -> да.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите размер массива: ");
+int n = int.Parse(ReadLine()!);
+int [] arr = GetArray(n);
+WriteLine("Введите число: ");
+int.TryParse(ReadLine(), out int number);
+
+printArray(arr);
+WriteLine();
+WriteLine(ArrayLookUp(arr,number)? $"число {number} присутствует в массиве" : $"заданного числа {number} нет в массиве");
+
+bool ArrayLookUp(int[] inArray, int num)
+{
+    foreach (var i in inArray)
+    {
+        if(num==i) return true;
+    }
+    return false;
+}
+
+int[] GetArray(int size)
+{
+int[] resultArray = new int [size]; 
+ 
+    Random random = new Random(); 
+    for (int i = 0; i < size; i++) 
+    { 
+        resultArray[i] = random.Next(0, 100); 
+    } 
+ 
+    return resultArray; 
+}
+
+void printArray(int[] array){ 
+    Write("["); 
+    for (int i = 0; i < array.Length - 1; i++) 
+    { 
+        Write($"{array[i]}, "); 
+    } 
+ 
+    Write($"{array[array.Length - 1]}]"); 
+}
