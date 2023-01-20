e04d479098569bf5ae8d8399347c26ff2c8c97c6
Nataly <lalliana@list.ru>
2023-01-20 16:58:39 +0300

    Произведение пар (1 и посл. эл-т) в массиве

---
+++ Seminar5/Task4/Program.cs
@@ -0,0 +1,46 @@
+﻿//Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,
+ //второй и предпоследний и т.д. Результат запишите в новом массиве. 
+ 
+using static System.Console; 
+Clear(); 
+ 
+int[] array = GetArray(11);  
+printArray(array); 
+int[] array2 = multiPara(array); 
+printArray(array2); 
+ 
+int [] GetArray(int size){ 
+     
+    int[] resultArray = new int [size]; 
+ 
+    Random random = new Random(); 
+    for (int i = 0; i < size; i++) 
+    { 
+        resultArray[i] = random.Next(0, 10); 
+    } 
+ 
+    return resultArray; 
+} 
+ 
+int[] multiPara(int[] array){ 
+    int size = array.Length / 2; 
+    if(array.Length % 2 == 1) size++; 
+ 
+    int[] result = new int[size]; 
+    for (int i = 0; i < size; i++) 
+    { 
+        result[i] = array[i] * array[array.Length - i - 1]; 
+    } 
+ 
+    return result; 
+} 
+ 
+void printArray(int[] array){ 
+    Write("["); 
+    for (int i = 0; i < array.Length - 1; i++) 
+    { 
+        Write($"{array[i]}, "); 
+    } 
+ 
+    WriteLine($"{array[array.Length - 1]}]"); 
+}
