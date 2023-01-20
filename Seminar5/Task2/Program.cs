42139f5463d313e1d82d5942fe40dd43a30a6abb
Nataly <lalliana@list.ru>
2023-01-20 16:54:12 +0300

    Замена положит. эл-в массива на отриц-е и наоборот

--- Seminar5/Task2/Program.cs
+++ Seminar5/Task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на 
+//соответствующие отрицательные, и наоборот.
+
+using static System.Console;
+Clear();
+
+int [] array = {-95, -5, -2, 0, 4, 8, 32};
+
+ReverseArray(array);
+PrintArray(array);
+
+void ReverseArray(int[] inarray)
+{
+    for (int i = 0; i < inarray.Length; i++)    
+    {
+        inarray[i] *= -1;
+    }
+}
+
+void PrintArray (int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length-1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length-1]}]");
+}
