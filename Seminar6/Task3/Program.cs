83e273ba8be8968ccd648600fd4f8b6629748239
Nataly <lalliana@list.ru>
2023-01-25 17:21:06 +0300

    Преобразование из десятичной системы в двоичную

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// **Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+using static System.Console;
+Clear();
+
+int num = AskUserForNumber("Введите число для перевода в двоичную систему: ");
+WriteLine($"\nДесятичное число {num} в двоичном исчислении будет {decimalToBinary(num)}.\n");
+
+string decimalToBinary(int inNum)
+{
+    if (inNum ==0) return "0";
+    string result = string.Empty;
+    //остаток - remainder -> var rem
+    int rem;
+    while (inNum > 0)
+    {
+        rem = inNum %2;
+        inNum /=2;
+        result = rem.ToString() + result;
+    }
+    return result;
+}
+
+int AskUserForNumber (string inPrompt)
+{
+    int resultNum = 0;
+    string userInput = "";
+    while (!int.TryParse(userInput, out resultNum))
+    {
+        Write (inPrompt);
+        userInput = ReadLine()!;
+    }
+    return resultNum;
+}
