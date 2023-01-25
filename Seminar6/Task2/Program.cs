f005fb816b8ea75f17f8d7214e3faaf91575aa38
Nataly <lalliana@list.ru>
2023-01-25 17:22:49 +0300

    Сущ-т ли треугольник со сторонами указанной длины

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник с сторонами такой длины.
+
+using static System.Console;
+Clear();
+
+int a = AskUserForNumber("Введите сторону треугольника а: ");
+int b = AskUserForNumber("Введите сторону треугольника b: ");
+int c = AskUserForNumber("Введите сторону треугольника c: ");
+
+WriteLine(IsTriangle (a, b, c) ? $"\nТреугольник со сторонами {a}, {b}, {c} существует.\n" : $"\n Треугольника со сторонами {a}, {b}, {c} не существует.\n");
+
+bool IsTriangle(int inA, int inB, int inC)
+{
+    return (inA + inB > inC && inA +inC >inB && inB + inC > inA);
+}
+
+int AskUserForNumber(string inPrompt)
+{
+    int resultNum = 0;
+    string userInput = "";
+    while (!int.TryParse(userInput, out resultNum))
+    {
+        Write(inPrompt);
+        userInput = ReadLine()!;
+    }
+    return resultNum;
+}
