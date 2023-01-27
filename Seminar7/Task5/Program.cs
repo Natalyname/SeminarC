7ae16c7fe129f18c265a8da3abb1ebf87c9d5541
Nataly <lalliana@list.ru>
2023-01-27 22:37:04 +0300

    Задача про цвета шахматной доски

---
+++ Seminar7/Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Какого цвета клетка шахматной доски, если начинается с черной
+
+using static System.Console;
+Clear();
+
+Write("Введите адрес клетки: ");
+int[] addres = Array.ConvertAll(ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+WriteLine(isBlack(addres) ? "Черная" : "Белая");
+
+bool isBlack (int[] addres){
+    if(addres[0] %2 == 0) return addres[1] % 2 == 0;
+    else return addres[1] % 2 != 0;
+}
