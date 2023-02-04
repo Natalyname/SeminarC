f48f0a51fd8acef2581fc6099acca4097baf3ec0
Nataly <lalliana@list.ru>
2023-02-04 23:06:51 +0300

    Сумма цифр числа рекурсией

---
+++ Seminar9/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿//**Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//  453 -> 12
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int N = int.Parse(ReadLine()!);
+
+GetNum(N);
+WriteLine($"Сумма цифр числа {N} равна {GetNum(N)}");
+
+int GetNum (int num)
+{
+if (num == 0) return num;
+else return GetNum (num/10) + num % 10;
+}
